f0eba5bd6ac6b7e0c871db1f763afc3737b3a314
r0527606571@gmail.com <r0527606571@gmail.com>
2025-03-10 13:46:15 +0200

    my project

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+@Component({
+    template: "<h1>hello!!!!!!</h1>",
+    selector: "my-app-root"
+})
+export class AppComponent {
+    
+
+    title: string = "Hello My - App ";
+
+    calc() {
+        return 0;
+    }
+
+    constructor() {
+        
+    }
+
+}
